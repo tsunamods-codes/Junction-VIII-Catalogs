b6786fc9ee06e192dc7f059dfe6b5e0e6473de16
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2025-11-29 17:56:15 +0100

    CI: Validate catalog using Junction-VIII code directly

---
+++ .github/workflows/prepare.ps1
@@ -0,0 +1,43 @@
+if ($env:_BUILD_BRANCH -eq "refs/heads/master" -Or $env:_BUILD_BRANCH -eq "refs/tags/canary") {
+  $env:_IS_BUILD_CANARY = "true"
+  $env:_IS_GITHUB_RELEASE = "true"
+}
+elseif ($env:_BUILD_BRANCH -like "refs/tags/*") {
+  $env:_BUILD_VERSION = $env:_BUILD_VERSION.Substring(0, $env:_BUILD_VERSION.LastIndexOf('.')) + ".0"
+  $env:_IS_GITHUB_RELEASE = "true"
+}
+$env:_RELEASE_VERSION = "v${env:_BUILD_VERSION}"
+
+Write-Output "--------------------------------------------------"
+Write-Output "RELEASE VERSION: $env:_RELEASE_VERSION"
+Write-Output "--------------------------------------------------"
+
+Write-Output "_BUILD_VERSION=${env:_BUILD_VERSION}" >> ${env:GITHUB_ENV}
+Write-Output "_RELEASE_VERSION=${env:_RELEASE_VERSION}" >> ${env:GITHUB_ENV}
+Write-Output "_IS_BUILD_CANARY=${env:_IS_BUILD_CANARY}" >> ${env:GITHUB_ENV}
+Write-Output "_IS_GITHUB_RELEASE=${env:_IS_GITHUB_RELEASE}" >> ${env:GITHUB_ENV}
+
+# Combine XML files
+$finalXml = "tsunamods.xml"
+Set-Content -Path $finalXml -Value '<?xml version="1.0"?>'
+Add-Content -Path $finalXml -Value '<Catalog Name="Tsunamods Catalog" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+Add-Content -Path $finalXml -Value '<Mods>'
+
+# Loop over XML files
+$files = Get-ChildItem -Path "mods" -Recurse -Filter *.xml
+foreach ($file in $files) {
+    # Read the mod.xml
+    $xml = Get-Content -LiteralPath $file.FullName -Raw
+
+    # Remove the initial xml tag
+    $xml = $xml.Replace('<?xml version="1.0"?>', '')
+
+    # Add content to final xml
+    Add-Content -Path $finalXml -Value $xml
+}
+
+Add-Content -Path $finalXml -Value '</Mods>'
+Add-Content -Path $finalXml -Value '</Catalog>'
+
+# Lint the final XML
+dotnet run --project app/CatalogValidator $finalXml
